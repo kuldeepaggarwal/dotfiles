baff6c358adbea8b2b9b9b8d0999e72f8755413f
Kuldeep Aggarwal <kuldeep.a.aggarwal@gmail.com>
2021-12-13 23:00:45 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+echo "Linking gitconfig..."
+rm -rf $HOME/.gitconfig
+ln -s $PWD/.gitconfig $HOME/.gitconfig
+
+echo "Installing spin specific tools..."
+if [ $SPIN ]; then
+  echo "Configure gpg keys..."
+  gpgconf --launch dirmngr
+  gpg --keyserver keys.openpgp.org --recv 29E3E915B231B9F5
+  
+  echo "source $PWD/.bash_aliases" >> $HOME/.zshrc
+fi
+
